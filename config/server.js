e6e0079b8f440e15954f842bd3f81fd5be815b1d
KJEKcode <kurtiskovel@gmail.com>
2021-08-10 20:09:06 -0400

    strapi swap

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET'),
+      secret: env('ADMIN_JWT_SECRET', '507f6bb4ffcdcb9a113699987b807b6c'),
     },
   },
 });
--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '922f8894-ca61-4c5c-b985-9e3a37609e67'
+  jwtSecret: process.env.JWT_SECRET || '4a4a947c-c337-4e7e-b9e3-6b5f65208961'
 };
